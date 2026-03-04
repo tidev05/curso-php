1845d4a64037ea6a74bbdffb4de5f90b10d6a72f
tidev05 <timatrixdev@gmail.com>
2026-03-04 15:57:27 -0300

    Ensino de funções

---
+++ funcoes.php
@@ -0,0 +1,23 @@
+<?php
+/*$lista = array(
+    "gabriel",
+    "joao",
+    "maria",
+    "pedro",
+    "ana",
+    "lucas",
+    "carolina",
+    "rafael",
+    "fernanda",
+    "thiago"
+);
+
+$convidados = "gabriel";
+
+function verificarConvidados($convidados, $lista)
+{
+    echo "Bem-vindo(a) " . $convidados . "!\n";
+    return $convidados;
+}
+verificarConvidados($convidados, $lista);
+*/
