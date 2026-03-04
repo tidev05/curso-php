9ef90e3b0f7eaad2417f963c48110bbf60854e66
tidev05 <timatrixdev@gmail.com>
2026-03-04 15:23:02 -0300

    laços

---
+++ lacos.php
@@ -0,0 +1,70 @@
+<?php
+/* while moderado
+$numero = 0;
+
+while ($numero < 100):
+
+    echo $numero++ . "\n";
+endwhile;*/
+$numero = 0;
+
+/* forma tradicional
+while ($numero < 200) {
+
+    echo $numero++ . "\n";
+}*/
+
+/*$numero = 0;
+
+do {
+    echo $numero++ . "\n";
+} while ($numero < 300);*/
+
+$numero = 0;
+
+/*for ($numero = 0; $numero < 500; $numero++) {
+    if ($numero == 200) {
+        break;
+    }
+    echo $numero . "\n";
+}*/
+
+/*for ($numero = 0; $numero < 500; $numero++) {
+    if ($numero % 2 == 0) {
+        continue;
+    }
+    echo $numero . "\n";
+}*/
+
+/*for ($numero = 0; $numero < 600; $numero++) {
+    if ($numero % 2 == 1) {
+        break;
+    }
+    echo $numero . "\n";
+}
+/*Falta para encerrar o laço foreach
+$menu = array("Home", "Sobre", "Contato", "Sair");
+
+foreach ($menu as $item) {
+
+    echo $item . "\n";
+}*/
+
+/* switch de decisão de varias escolhas
+$alvo = 1;
+$inicio = 3;
+
+switch ($alvo) {
+    case $inicio - 1:
+        print "A";
+        break;
+    case $inicio - 2:
+        print "B";
+        break;
+    case $inicio - 3:
+        print "C";
+        break;
+    case $inicio - 4:
+        print "D";
+        break;
+}*/
